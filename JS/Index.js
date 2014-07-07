ff536ca2a0f131ccb933f54c1e1bb228863be177
Michael Stapleton <mgstapleton@gmail.com>
2014-07-07 23:27:32 +0100

    geolocation added

--- JS/Index.js
+++ JS/Index.js
@@ -4,13 +4,14 @@
 //
 
 $(document).ready(function () {
+    var locID = null;
     console.log("loaded index.js");
 	addClub();
 	createDb();
     $(logo).attr('align', 'absmiddle');
     dateFormat();
 	bounds = new google.maps.LatLngBounds();
-
+    locID = navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	if (typeof jQuery === "undefined") {
 		alert("Jquery not present");
 	}
@@ -329,6 +330,25 @@ function bindInfoWindow(marker, map, infowindow, strDescription) {
         infowindow.open(map, marker);
     });
 }
+
+function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          +                                   position.timestamp          + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
 
 // These functions are exposed as a module named "locator"
 var locator = (function () {
